3b54bea7d40438bf72fd6c5404c6b21b1e4b7f7f
Ivan Borisenko <myivanbor@gmail.com>
2023-07-21 02:12:15 +0400

    Решешие задач сем-1

---
+++ Sem_1/program.java
@@ -0,0 +1,10 @@
+/**
+* Program 
+*/
+public class program {
+public static void main(String[] args) {
+    System.out.println("bye world");
+    String s = null;    // определяется парой
+    System.out.println(s);  //определяется парой
+}
+}
